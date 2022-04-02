242c64adecbe3432d4de628085862dbc7a111336
22000603 <22000603@handong.edu>
2022-04-02 11:00:18 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/samsonapp/generated2widget/Generated2Widget.dart';
+import 'package:flutterapp/samsonapp/generated2widget2/Generated2Widget2.dart';
+import 'package:flutterapp/samsonapp/generated2widget4/Generated2Widget4.dart';
+import 'package:flutterapp/samsonapp/generated2widget5/Generated2Widget5.dart';
+import 'package:flutterapp/samsonapp/generated2widget6/Generated2Widget6.dart';
+import 'package:flutterapp/samsonapp/generatedwidget170/GeneratedWidget170.dart';
+import 'package:flutterapp/samsonapp/generatedwidget213/GeneratedWidget213.dart';
+import 'package:flutterapp/samsonapp/generatedwidget267/GeneratedWidget267.dart';
+import 'package:flutterapp/samsonapp/generated2widget7/Generated2Widget7.dart';
+import 'package:flutterapp/samsonapp/generated2widget8/Generated2Widget8.dart';
+import 'package:flutterapp/samsonapp/generated2widget9/Generated2Widget9.dart';
+import 'package:flutterapp/samsonapp/generated2widget10/Generated2Widget10.dart';
+import 'package:flutterapp/samsonapp/generatedwidget472/GeneratedWidget472.dart';
+import 'package:flutterapp/samsonapp/generated2widget11/Generated2Widget11.dart';
+import 'package:flutterapp/samsonapp/generated2widget12/Generated2Widget12.dart';
+import 'package:flutterapp/samsonapp/generated2widget14/Generated2Widget14.dart';
+import 'package:flutterapp/samsonapp/generated2newwidget/Generated2NewWidget.dart';
+import 'package:flutterapp/samsonapp/generated1widget14/Generated1Widget14.dart';
+import 'package:flutterapp/samsonapp/generated5widget7/Generated5Widget7.dart';
+import 'package:flutterapp/samsonapp/generated4widget4/Generated4Widget4.dart';
+import 'package:flutterapp/samsonapp/generated4widget6/Generated4Widget6.dart';
+import 'package:flutterapp/samsonapp/generated4widget8/Generated4Widget8.dart';
+import 'package:flutterapp/samsonapp/generated4newwidget/Generated4NewWidget.dart';
+import 'package:flutterapp/samsonapp/generated4widget11/Generated4Widget11.dart';
+import 'package:flutterapp/samsonapp/generated3widget9/Generated3Widget9.dart';
+import 'package:flutterapp/samsonapp/generated3widget10/Generated3Widget10.dart';
+import 'package:flutterapp/samsonapp/generated3widget11/Generated3Widget11.dart';
+import 'package:flutterapp/samsonapp/generated3widget12/Generated3Widget12.dart';
+import 'package:flutterapp/samsonapp/generated3widget13/Generated3Widget13.dart';
+import 'package:flutterapp/samsonapp/generatedwidget646/GeneratedWidget646.dart';
+import 'package:flutterapp/samsonapp/generatedwidget647/GeneratedWidget647.dart';
+import 'package:flutterapp/samsonapp/generatedwidget648/GeneratedWidget648.dart';
+import 'package:flutterapp/samsonapp/generatedcommentoriginalwidget7/GeneratedCommentoriginalWidget7.dart';
+import 'package:flutterapp/samsonapp/generateddayoriginalwidget/GeneratedDayoriginalWidget.dart';
+import 'package:flutterapp/samsonapp/generateddayselectwidget5/GeneratedDayselectWidget5.dart';
+import 'package:flutterapp/samsonapp/generateddaycheckwidget/GeneratedDaycheckWidget.dart';
+import 'package:flutterapp/samsonapp/generatedcontentoriginalwidget10/GeneratedContentoriginalWidget10.dart';
+import 'package:flutterapp/samsonapp/generatedwidget649/GeneratedWidget649.dart';
+import 'package:flutterapp/samsonapp/generated2widget28/Generated2Widget28.dart';
+import 'package:flutterapp/samsonapp/generated2widget29/Generated2Widget29.dart';
+
+void main() {
+  runApp(SAMSONApp());
+}
+
+class SAMSONApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedWidget648',
+      routes: {
+        '/Generated2Widget': (context) => Generated2Widget(),
+        '/Generated2Widget2': (context) => Generated2Widget2(),
+        '/Generated2Widget4': (context) => Generated2Widget4(),
+        '/Generated2Widget5': (context) => Generated2Widget5(),
+        '/Generated2Widget6': (context) => Generated2Widget6(),
+        '/GeneratedWidget170': (context) => GeneratedWidget170(),
+        '/GeneratedWidget213': (context) => GeneratedWidget213(),
+        '/GeneratedWidget267': (context) => GeneratedWidget267(),
+        '/Generated2Widget7': (context) => Generated2Widget7(),
+        '/Generated2Widget8': (context) => Generated2Widget8(),
+        '/Generated2Widget9': (context) => Generated2Widget9(),
+        '/Generated2Widget10': (context) => Generated2Widget10(),
+        '/GeneratedWidget472': (context) => GeneratedWidget472(),
+        '/Generated2Widget11': (context) => Generated2Widget11(),
+        '/Generated2Widget12': (context) => Generated2Widget12(),
+        '/Generated2Widget14': (context) => Generated2Widget14(),
+        '/Generated2NewWidget': (context) => Generated2NewWidget(),
+        '/Generated1Widget14': (context) => Generated1Widget14(),
+        '/Generated5Widget7': (context) => Generated5Widget7(),
+        '/Generated4Widget4': (context) => Generated4Widget4(),
+        '/Generated4Widget6': (context) => Generated4Widget6(),
+        '/Generated4Widget8': (context) => Generated4Widget8(),
+        '/Generated4NewWidget': (context) => Generated4NewWidget(),
+        '/Generated4Widget11': (context) => Generated4Widget11(),
+        '/Generated3Widget9': (context) => Generated3Widget9(),
+        '/Generated3Widget10': (context) => Generated3Widget10(),
+        '/Generated3Widget11': (context) => Generated3Widget11(),
+        '/Generated3Widget12': (context) => Generated3Widget12(),
+        '/Generated3Widget13': (context) => Generated3Widget13(),
+        '/GeneratedWidget646': (context) => GeneratedWidget646(),
+        '/GeneratedWidget647': (context) => GeneratedWidget647(),
+        '/GeneratedWidget648': (context) => GeneratedWidget648(),
+        '/GeneratedCommentoriginalWidget7': (context) =>
+            GeneratedCommentoriginalWidget7(),
+        '/GeneratedDayoriginalWidget': (context) =>
+            GeneratedDayoriginalWidget(),
+        '/GeneratedDayselectWidget5': (context) => GeneratedDayselectWidget5(),
+        '/GeneratedDaycheckWidget': (context) => GeneratedDaycheckWidget(),
+        '/GeneratedContentoriginalWidget10': (context) =>
+            GeneratedContentoriginalWidget10(),
+        '/GeneratedWidget649': (context) => GeneratedWidget649(),
+        '/Generated2Widget28': (context) => Generated2Widget28(),
+        '/Generated2Widget29': (context) => Generated2Widget29(),
+      },
+    );
+  }
+}
